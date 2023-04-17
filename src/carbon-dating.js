7602d903e61a7cbe55f80b11e55c4ca658eaf5dd
NecomaV <beken200225@gmail.com>
2023-04-17 14:22:56 +0600

    feat: carbon -dating

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -18,10 +18,21 @@ const HALF_LIFE_PERIOD = 5730;
  *
  */
 function dateSample(sampleActivity) {
-  if(parseInt(sampleActivity) !== Number ){
+
+  let sample = parseFloat(sampleActivity);
+  if (typeof sampleActivity !== 'string' || isNaN(sample) || sample <= 0 || sample > MODERN_ACTIVITY) {
     return false;
   }
+
+  let k = 0.693 / HALF_LIFE_PERIOD;
+  let t = Math.log(MODERN_ACTIVITY / sample) / k;
+  return Math.ceil(t);
+  
+
+
 }
+
+
 
 module.exports = {
   dateSample
